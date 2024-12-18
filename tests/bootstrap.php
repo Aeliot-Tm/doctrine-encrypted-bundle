d0f9571f91370509693aec6d0e33d827ed29272f
Anatoliy Melnikov <5785276@gmail.com>
2024-12-18 21:32:01 +0300

    Add bootstrap file for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TransMaintain.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require_once dirname(__DIR__) . '/app/bootstrap.php';
